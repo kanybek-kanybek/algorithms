b586d5dc78b06763780d765f7dafbe0052323249
kanybek-kanybek <145540346+kanybek-kanybek@users.noreply.github.com>
2024-09-06 16:21:33 +0600

    Time: 60 ms (42.57%), Space: 51448000 (26.98%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.js
@@ -0,0 +1,26 @@
+var addBinary = function(a, b) {
+    let result = "";  
+    let carry = 0;   
+    let i = a.length - 1; 
+    let j = b.length - 1; 
+    
+    while (i >= 0 || j >= 0 || carry) {
+        let sum = carry; 
+        
+        if (i >= 0) {
+            sum += parseInt(a[i]);
+            i--;
+        }
+        
+        if (j >= 0) {
+            sum += parseInt(b[j]);
+            j--;
+        }
+        
+        result = (sum % 2) + result;
+        
+        carry = Math.floor(sum / 2);
+    }
+    
+    return result;
+};
